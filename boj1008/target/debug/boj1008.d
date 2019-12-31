47eaaf302ea2c555647af7fa80f8eec2d94717a5
saechimdaeki <anima94@naver.com>
2019-12-31 17:15:53 +0900

    rust 언어 좀 그렇네 ;;

---
+++ boj1008/target/debug/boj1008.d
@@ -0,0 +1,1 @@
+c:\Users\anima\Desktop\Study_all_by_myself_-GO-\boj1008\target\debug\boj1008.exe: c:\Users\anima\Desktop\Study_all_by_myself_-GO-\boj1008\src\main.rs
---
+++ boj1008/target/rls/debug/deps/boj1008-091076fc257daa8d.d
@@ -0,0 +1,5 @@
+c:\Users\anima\Desktop\Study_all_by_myself_-GO-\boj1008\target\rls\debug\deps\boj1008-091076fc257daa8d.rmeta: src\main.rs
+
+c:\Users\anima\Desktop\Study_all_by_myself_-GO-\boj1008\target\rls\debug\deps\boj1008-091076fc257daa8d.d: src\main.rs
+
+src\main.rs:
